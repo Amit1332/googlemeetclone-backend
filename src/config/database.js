311ed2a2ca870614ec92fa3a8f57c6d5e02b17e3
Amitesh <amitsinghpatel9747@gmail.com>
2025-06-20 09:40:50 +0530

    updated mongouri

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const { SUCCESS_MESSAGES, ERROR_MESSAGES } = require('../helper/messages')
 
 const dbConnection = async () => {
-    await mongoose.connect('mongodb+srv://amitsinghpatel9747:bNFQz8kfIZBFcSsn@cluster0.tpywbad.mongodb.net/meetclone?retryWrites=true&w=majority').then(() => {
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
          console.log(`${SUCCESS_MESSAGES.DATABASE_CONNECTION}`)
 }).catch((error) => {
          console.log(`${ERROR_MESSAGES.DATABASE_CONNECTION}`)
