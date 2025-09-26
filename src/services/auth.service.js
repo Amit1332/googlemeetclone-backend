9eca7ac826690529aeb3c9df39f154d48f6df5b2
Amitesh <amitsinghpatel9747@gmail.com>
2025-09-26 18:26:14 +0530

    fix api error

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,6 +2,7 @@ const { HTTP_STATUS_CODES } = require("@simple-node/http-status-codes");
 const bcrypt = require("bcryptjs");
 const User = require("../model/user.schema");
 const { ERROR_MESSAGES } = require("../helper/messages");
+const ApiError = require("../utils/ApiError");
 
 const loginUserWithEmailAndPassword = async (email, password) => {
   let userInfo = await User.findOne({ email }).select("+password"); // include password only for comparison
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -12,41 +12,15 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req, file) => {
     try {
-      const ext = file.originalname.split(".").pop().toLowerCase();
-
-      const imageFormats = ["jpg", "jpeg", "png", "gif", "webp", "svg", "bmp"];
-      const videoFormats = ["mp4", "mov", "avi", "mkv"];
-      const rawFormats = ["pdf", "doc", "docx", "xls", "xlsx", "csv", "xlsm", "ods", "zip", "rar", "txt"];
-      const audioFormats = ["mp3", "wav", "ogg", "m4a"];
-
-      let resource_type = "image"; // default
-
-      if (videoFormats.includes(ext) || audioFormats.includes(ext)) {
-        resource_type = "video";
-      } else if (rawFormats.includes(ext)) {
-        resource_type = "raw";
-      }
-
-      const baseParams = {
+      return {
         folder: "chat_uploads",
-        resource_type,
+        resource_type: "auto", // automatically detect (image, video, raw, audio, pdf, zip, etc.)
       };
-
-      // Only add allowed_formats and transformation for images/videos
-      if (resource_type === "image") {
-        baseParams.allowed_formats = imageFormats;
-        baseParams.transformation = [{ width: 800, height: 800, crop: "limit" }];
-      } else if (resource_type === "video") {
-        baseParams.allowed_formats = [...videoFormats, ...audioFormats]
-      }
-
-      return baseParams;
     } catch (error) {
       console.error("Error in Cloudinary params:", error);
       throw new Error("Failed to set Cloudinary params");
     }
   },
 });
-;
 
 module.exports = { cloudinary, storage };
